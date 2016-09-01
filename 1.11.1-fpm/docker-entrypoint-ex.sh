1fd39f58e0350d7224c6b311eae72d09fe60fa7d
@ontheroad_jp <dev@ontheroad.jp>
2016-09-01 18:27:55 +0900

    [Fix] setting GID and UID

--- 1.11.1-fpm/docker-entrypoint-ex.sh
+++ 1.11.1-fpm/docker-entrypoint-ex.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
 
-usermod -u ${TOYBOX_UID} nginx
-groupmod -g ${TOYBOX_GID} nginx
+if [ -n "${TOYBOX_GID+x}" ]; then
+    groupmod -g ${TOYBOX_GID} nginx
+fi
+
+if [ -n "${TOYBOX_UID+x}" ]; then
+    usermod -u ${TOYBOX_UID} nginx
+fi
 
 docroot="/var/www/html"
-#echo "extract ${docroot}"
-#tar xvzf /usr/src/nginx-default-doc.tar.gz -C ${docroot}
+mkdir -p ${docroot}
 chown -R nginx:nginx ${docroot}
 
 confdir="/etc/nginx"
+mkdir -p ${confdir}
 echo "extract ${confdir}"
 tar xvzf /usr/src/nginx-conf.tar.gz -C ${confdir}
 cp /default.conf /etc/nginx/conf.d
