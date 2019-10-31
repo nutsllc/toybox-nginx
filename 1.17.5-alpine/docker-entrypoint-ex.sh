d83436897b2483d219cb9cd14b9da519b67ce5c8
@ontheroad_jp <dev@ontheroad.jp>
2019-10-31 19:30:20 +0900

    [Update] impliment version generator

--- 1.17.5-alpine/docker-entrypoint-ex.sh
+++ 1.17.5-alpine/docker-entrypoint-ex.sh
@@ -28,49 +28,49 @@ fi
 
 
 
-## -----------------------------
-## document root
-## -----------------------------
-#: ${DOCUMENT_ROOT:="/usr/share/nginx/html"}
-#mkdir -p ${DOCUMENT_ROOT}
-#if [ $(ls "${DOCUMENT_ROOT}" | wc -l) -eq 0 ] && [ -n "${PHP_FPM_HOST}" ]; then
-#    echo "<?php phpinfo(); ?>" > ${DOCUMENT_ROOT}/index.php
-#elif [ $(ls "${DOCUMENT_ROOT}" | wc -l) -eq 0 ]; then
-#    tar xzf /usr/src/nginx-default-doc.tar.gz -C ${DOCUMENT_ROOT} && {
-#        echo "extract ${DOCUMENT_ROOT}"
-#    }
-#fi
-#chown -R ${user}:${group} ${DOCUMENT_ROOT}
+# -----------------------------
+# document root
+# -----------------------------
+: ${DOCUMENT_ROOT:="/usr/share/nginx/html"}
+mkdir -p ${DOCUMENT_ROOT}
+if [ $(ls "${DOCUMENT_ROOT}" | wc -l) -eq 0 ] && [ -n "${PHP_FPM_HOST}" ]; then
+    echo "<?php phpinfo(); ?>" > ${DOCUMENT_ROOT}/index.php
+elif [ $(ls "${DOCUMENT_ROOT}" | wc -l) -eq 0 ]; then 
+    tar xzf /usr/src/nginx-default-doc.tar.gz -C ${DOCUMENT_ROOT} && {
+        echo "extract ${DOCUMENT_ROOT}"
+    }
+fi
+chown -R ${user}:${group} ${DOCUMENT_ROOT}
 
-## -----------------------------
-## config
-## -----------------------------
-#confbase="/etc/nginx"
-#conf_dir=${confbase}/conf.d
-#
-#mkdir -p ${confbase}
-#if [ $(ls "${confbase}" | wc -l) -eq 0 ]; then
-#    tar xzf /usr/src/nginx-conf.tar.gz -C ${confbase} && {
-#        sed -i -e "s:/usr/share/nginx/html:${DOCUMENT_ROOT}:g" ${conf_dir}/default.conf
-#        sed -i -e "s:\(.*root \{11\}\)html:\1${DOCUMENT_ROOT}:g" ${conf_dir}/default.conf
-#        echo "extract ${confbase}"
-#    }
-#fi
+# -----------------------------
+# config
+# -----------------------------
+confbase="/etc/nginx"
+conf_dir=${confbase}/conf.d
 
-## -----------------------------
-## PHP_FPM_HOST
-## -----------------------------
-#if [ -n "${PHP_FPM_HOST}" ]; then
-#    [ -f ${conf_dir}/default.conf ] && {
-#        rm ${conf_dir}/default.conf
-#        mv /default-fpm.conf ${conf_dir}/default-fpm.conf
-#    }
-#    sed -i -e "s/\(fastcgi_pass   \)127.0.0.1:9000/\1${PHP_FPM_HOST}/" ${conf_dir}/default-fpm.conf
-#    sed -i -e "s:/usr/share/nginx/html:${DOCUMENT_ROOT}:g" ${conf_dir}/default-fpm.conf
-#    sed -i -e "s:\(.*root \{11\}\)html:\1${DOCUMENT_ROOT}:g" ${conf_dir}/default-fpm.conf
-#fi
-#
-#chown -R ${user}:${group} ${confbase}
+mkdir -p ${confbase}
+if [ $(ls "${confbase}" | wc -l) -eq 0 ]; then 
+    tar xzf /usr/src/nginx-conf.tar.gz -C ${confbase} && {
+        sed -i -e "s:/usr/share/nginx/html:${DOCUMENT_ROOT}:g" ${conf_dir}/default.conf
+        sed -i -e "s:\(.*root \{11\}\)html:\1${DOCUMENT_ROOT}:g" ${conf_dir}/default.conf
+        echo "extract ${confbase}"
+    }
+fi
+
+# -----------------------------
+# PHP_FPM_HOST
+# -----------------------------
+if [ -n "${PHP_FPM_HOST}" ]; then
+    [ -f ${conf_dir}/default.conf ] && {
+        rm ${conf_dir}/default.conf
+        mv /default-fpm.conf ${conf_dir}/default-fpm.conf
+    }
+    sed -i -e "s/\(fastcgi_pass   \)127.0.0.1:9000/\1${PHP_FPM_HOST}/" ${conf_dir}/default-fpm.conf
+    sed -i -e "s:/usr/share/nginx/html:${DOCUMENT_ROOT}:g" ${conf_dir}/default-fpm.conf
+    sed -i -e "s:\(.*root \{11\}\)html:\1${DOCUMENT_ROOT}:g" ${conf_dir}/default-fpm.conf
+fi
+
+chown -R ${user}:${group} ${confbase}
 
 # -----------------------------
 # exec
